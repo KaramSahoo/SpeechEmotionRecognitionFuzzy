c4b03a74e3b97cb21b40e13e7d3fc5c4b288fb03
KaramSahoo <51832871+KaramSahoo@users.noreply.github.com>
2021-11-22 01:47:08 +0530

    Update split.py

--- Scripts/split.py
+++ Scripts/split.py
@@ -1,2 +1,2 @@
 import splitfolders
-splitfolders.ratio("../MELS", output="../mel_data", seed=1337, ratio=(.8, .2), group_prefix=None)
+splitfolders.ratio("../MELS", output="../mel_data", seed=1337, ratio=(.8, .1, .1), group_prefix=None)
